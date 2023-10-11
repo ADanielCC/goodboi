a9d43316f5b82c43240ef4c9033292c97d98d75e
Daniel Adolfsson <daniel.adolfsson.93@gmail.com>
2023-10-11 07:25:07 +0200

    added pricing component and button

--- src/components/ContentManager/ContentManager.jsx
+++ src/components/ContentManager/ContentManager.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Services from './Services/Services';
 import AboutUs from './AboutUs/AboutUs';
 import Contact from './Contact/Contact';
+import Pricing from "./Pricing/Pricing";
 import Messaging from './Messaging/Messaging';
 import Gallery from './Gallery/Gallery';
 import Landing from "./Landing/Landing";
@@ -45,39 +46,46 @@ class ContentManager extends Component {
       <div className="content-manager">
 
         <div className="sections-wrapper">
-          
-        <div className="button-wrapper">
-  <button
-    onClick={() => this.setActiveSection("landing")}
-    className={activeSection === "landing" ? "active" : ""}
-  >
-    Start
-  </button>
-  <button
-    onClick={() => this.setActiveSection("about")}
-    className={activeSection === "about" ? "active" : ""}
-  >
-    Om oss
-  </button>
-  <button
-    onClick={() => this.setActiveSection("contact")}
-    className={activeSection === "contact" ? "active" : ""}
-  >
-    Info
-  </button>
-  <button
-    onClick={() => this.setActiveSection("messaging")}
-    className={activeSection === "messaging" ? "active" : ""}
-  >
-    Kontakta oss
-  </button>
-  <button
-    onClick={() => this.setActiveSection("gallery")}
-    className={activeSection === "gallery" ? "active" : ""}
-  >
-    Bilder
-  </button>
-</div>
+
+          <div className="button-wrapper">
+            <button
+              onClick={() => this.setActiveSection("landing")}
+              className={activeSection === "landing" ? "active" : ""}
+            >
+              På God Tass
+            </button>
+            <button
+              onClick={() => this.setActiveSection("about")}
+              className={activeSection === "about" ? "active" : ""}
+            >
+              Om oss
+            </button>
+
+            <button
+              onClick={() => this.setActiveSection("pricing")}
+              className={activeSection === "pricing" ? "active" : ""}
+            >
+              Våra tjänster
+            </button>
+            <button
+              onClick={() => this.setActiveSection("contact")}
+              className={activeSection === "contact" ? "active" : ""}
+            >
+              Info
+            </button>
+            <button
+              onClick={() => this.setActiveSection("messaging")}
+              className={activeSection === "messaging" ? "active" : ""}
+            >
+              Kontakta oss
+            </button>
+            <button
+              onClick={() => this.setActiveSection("gallery")}
+              className={activeSection === "gallery" ? "active" : ""}
+            >
+              Bilder
+            </button>
+          </div>
 
 
 
@@ -86,6 +94,7 @@ class ContentManager extends Component {
 
           {activeSection === "landing" && <Landing className="content" {...commonProps} />}
           {activeSection === "about" && <AboutUs className="content" {...commonProps} />}
+          {activeSection === "pricing" && <Pricing className="content" {...commonProps} setActiveSection={this.setActiveSection}/>}
           {activeSection === "contact" && <Contact className="content" {...commonProps} />}
           {activeSection === "messaging" && <Messaging className="content" {...commonProps} />}
           {activeSection === "gallery" && <Gallery className="content" images={images} {...commonProps} />}
---
+++ src/components/ContentManager/Pricing/Pricing.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import './Pricing.css';
+
+export default class Pricing extends Component {
+
+
+    handleContactClick = () => {
+        // Call the setActiveSection function to change the active section to "contact"
+        this.props.setActiveSection("messaging");
+      };
+  render() {
+
+    // const { className,redTitle,blueTitle} = this.props; // Get the className prop
+    const { className,redTitle} = this.props;
+    return (
+      <div ref={this.props.pricesRef}
+      className={className}
+            >
+        <h2 className={redTitle} >Vi erbjuder:</h2>
+        {/* <h4 className={redTitle}>Priser:</h4> */}
+        <p>Fr.o.m 1/11 2022</p>
+        <ul>
+       <li>Liten hund 250 kr per påbörjad dag, ytterligare liten hund i samma box 150kr </li>
+       <li>Stor hund 300 kr per påbörjat dygn , ytterligare hund i samma  box 150kr </li>
+       <li>Medicinering av hund , 50kr per dag </li>
+       <li>Månads abonnemang enligt överenskommelse</li>
+       </ul>
+       <p><a className="contact-link underline" onClick={this.handleContactClick}>Kontakta oss</a> gärna för mer information!</p>
+
+
+      </div>
+    );
+  }
+}
+
